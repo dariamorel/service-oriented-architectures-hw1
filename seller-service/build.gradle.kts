78a78d93880220f7b3739b80b885007a9b54ad64
Daria Morel <moreld.m.2005@mail.ru>
2026-02-19 21:16:10 +0300

    Реализован сервис с докером

---
+++ seller-service/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+    application
+}
+
+application {
+    mainClass.set("org.example.SellerServiceKt")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.3.12")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.12")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
