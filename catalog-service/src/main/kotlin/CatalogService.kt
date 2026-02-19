021415be0be5e13abbf07e2b392d525d8aec1a05
Daria Morel <moreld.m.2005@mail.ru>
2026-02-19 22:54:32 +0300

    правки

---
+++ catalog-service/src/main/kotlin/CatalogService.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+
+    embeddedServer(Netty, port = port, host = "0.0.0.0") {
+        routing {
+            get("/health") {
+                call.respondText("ok")
+            }
+        }
+    }.start(wait = true)
+}
